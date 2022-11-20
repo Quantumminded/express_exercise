8e9ec2e63e6b146338130955842ed1a6b43d9711
Luca <lukkino31@gmail.com>
2022-11-20 12:59:08 +0100

    changed res in index.js to sendFile

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1>');
+    res.sendFile('home.html', {root: __dirname + "/public"});
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
+    res.sendFile('about.html', {root: __dirname + "/public"});
 })
 
 app.get('/contact', (req, res) => {
-    res.send('<h1>Contact</h1>');
+    res.sendFile('contact.html', {root: __dirname + "/public"});
 })
 
 app.all('*', (req, res) => {
-    res.send('<h1>Page not found</h1>');
+    res.sendFile('404.html', {root: __dirname + "/public"});
 })
 app.listen(3000)
